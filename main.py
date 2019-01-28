4f7c326ee5285f5cb061b3d81f305a5a5ee8c891
Mark Zhang <z@cciex.com>
2019-01-28 08:36:11 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+'''
+this script is used to push my notes to github
+'''
+
+import os
+import argparse
+import time
+
+HOME_DIR = os.environ['HOME']
+DOCS_DIR = os.path.join(HOME_DIR, 'Documents')
+Notes_DIR_NAME = 'Notes'
+
+class GPush(object):
+
+    def __init__(self):
+        self.docs_dir = DOCS_DIR
+        self.notes_dir_list = []
+        self._load_notes_dir()
+
+    def _load_notes_dir(self):
+
+        dir_and_files = [os.path.join(self.docs_dir, n) for n in os.listdir(self.docs_dir)]
+        main_dirs = filter(os.path.isdir, dir_and_files)
+        #convert <filter object> to <list object>
+        main_dirs = list(main_dirs)
+
+        #Verify if .git exist
+
+        #codes...
+
+
+        for d in main_dirs:
+            if os.path.exists(os.path.join(d, Notes_DIR_NAME)):
+                self.notes_dir_list.append(os.path.join(d, Notes_DIR_NAME))
+
+        return len(self.notes_dir_list)
+
+    def convert_path(self, input_path):
+        p = os.path.abspath(input_path)
+        if os.path.exists(p):
+            return p
+        else:
+            print(f"The path <{input_path}> is invalid!")
+            return False
+
+    def push(self, target_dir, comment):
+        target_dir = self.convert_path(target_dir)
+        #determine if the dir contains .git
+        if not os.path.exists(os.path.join(target_dir, '.git')):
+            print(f'Directory: < {target_dir} > is not a git Dir!')
+            return False
+        try:
+            print(f'\n \nDirectory----> {target_dir}')
+            os.chdir(target_dir)
+
+            print(f'git adding...')
+            os.system('git add -A')
+
+            print(f'git commiting...   Comment: <{comment}>')
+            os.system(f'git commit -m "{comment}"')
+
+            print(f'git pushing...')
+            os.system('git push origin master')
+
+            return True
+
+        except Exception as e:
+            print(e)
+
+    def push_all(self, comment):
+        for target_dir in self.notes_dir_list:
+            self.push(target_dir, comment)
+        return len(self.notes_dir_list)
+
+
+def main():
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--all', help='Push all notes to github', action='store_true')
+    parser.add_argument('-d', '--dir', help='The target Dir you want to push', default='.')
+    parser.add_argument('comment', help='The git commit comments', nargs='?', default=f'Commit at {current_time}')
+    args = parser.parse_args()
+
+
+
+    gpush = GPush()
+
+    if args.all:
+        gpush.push_all(args.comment)
+    else:
+        gpush.push(args.dir,args.comment)
+
+
+if __name__ == '__main__':
+    main()
